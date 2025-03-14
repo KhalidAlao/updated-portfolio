7cf2d569b2d28d9b052d508ba3600f3a35962741
KhalidAlao <alaokha@gmail.com>
2025-03-14 10:30:09 +0000

    Projects Component

---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './Projects.module.css';
+import ProjectData from '../ProjectData/ProjectData';
+
+
+const Projects = () => {
+  return (
+    <section className={styles.projectsSection} id="projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projectsGrid}>
+        {ProjectData.map((project, index) => (
+          <div key={index} className={styles.projectCard}>
+            <img src={project.image} alt={project.title} className={styles.projectImage} />
+            <h3 className={styles.projectTitle}>{project.title}</h3>
+            <p className={styles.projectDescription}>{project.description}</p>
+            <a href={project.githubLink} className={styles.projectLink}>GitHub</a>
+            <a href={project.demoLink} className={styles.projectLink}>Live Demo</a>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
