b0fa3d009add33cacd54cf89a114da1c774243d5
KhalidAlao <alaokha@gmail.com>
2025-03-14 10:28:22 +0000

    ProjectData Array

---
+++ src/Components/ProjectData/ProjectData.js
@@ -0,0 +1,13 @@
+const ProjectData = [
+    {
+      title: "Breathwork Guide Simulation",
+      description: "A simple web app that simulates a breathwork guide with a visual breathing animation.",
+      image: "https://i.ibb.co/993HdN60/momo-studio-DXZj4-Ftk-H8w-unsplash.jpg",
+      githubLink: "https://github.com/KhalidAlao/breathwork-guide",
+      demoLink: "#" //placeholder until the demo of the project is built
+    },
+    // Add more projects here when built
+   
+  ];
+  
+  export default ProjectData;
