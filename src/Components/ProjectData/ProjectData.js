9da160add92ab787182a56c29132d030a017fc4d
KhalidAlao <alaokha@gmail.com>
2025-03-15 09:34:14 +0000

    Update ProjectData.js

--- src/Components/ProjectData/ProjectData.js
+++ src/Components/ProjectData/ProjectData.js
@@ -1,6 +1,6 @@
 const ProjectData = [
     {
-      title: "Breathwork Guide Simulation",
+      title: "4-7-8 Breathing Guide",
       description: "A guided breathing application built with React and Framer Motion, designed to help users practice the 4-7-8 breathing technique for relaxation.",
       image: "https://i.ibb.co/993HdN60/momo-studio-DXZj4-Ftk-H8w-unsplash.jpg",
       githubLink: "https://github.com/KhalidAlao/breathwork-guide",
