052ab2c8e2632027273d3ecde5719f11c4dd49c7
KhalidAlao <alaokha@gmail.com>
2025-03-18 07:53:50 +0000

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <section className={styles.aboutSection} id="about">
       <h2 className={styles.title}>About Me</h2>
       <p className={styles.description}>
-        I am a dedicated first-year computer science student based in London, interested in software development and eager to embrace new technologies that align with my expanding career goals.
+      Hi there! I’m a first-year computer science student based in London, with aspirations of working in software development and constantly exploring new technologies. Currently, I’m learning frontend development, with plans to expand my skills into full-stack development in the near future.
       </p>
     </section>
   );
