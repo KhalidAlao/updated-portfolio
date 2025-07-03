61d9f696cc575f70e2e701843f49294c130f5689
KhalidAlao <alaokha@gmail.com>
2025-07-03 18:40:19 +0100

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,9 +3,9 @@ import styles from './About.module.css';
 
 const About = () => {
   return (
-    <section className={styles.aboutSection} id="about">
-      <h2 className={styles.title}>About Me</h2>
-      <p className={styles.description}>
+    <section aria-labelledby="about-title" className={styles.aboutSection} id="about">
+      <h2 id="about-title" className={styles.title}>About Me</h2>
+      <p tabIndex="0" className={styles.description}>
       Hi there! I’m a first-year computer science student based in London, with aspirations of working in software development. Currently, I’m learning frontend development, with plans to expand my skills into full-stack development in the near future.
       </p>
     </section>
