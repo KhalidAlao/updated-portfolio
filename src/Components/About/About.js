8b96c46a3883efbc148bc00c891683378b1f76c9
KhalidAlao <alaokha@gmail.com>
2025-07-03 19:22:32 +0100

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,7 +6,7 @@ const About = () => {
     <section aria-labelledby="about-title" className={styles.aboutSection} id="about">
       <h2 id="about-title" className={styles.title}>About Me</h2>
       <p tabIndex="0" className={styles.description}>
-      Hi there! I’m a first-year computer science student based in London, with aspirations of working in software development. Currently, I’m learning frontend development, with plans to expand my skills into full-stack development in the near future.
+      Computer Science student at Brunel University with a passion for full-stack development, specializing in React, JavaScript, and RESTful APIs. I’ve built real-world projects like a 4-7-8 Breathing Guide (React, Framer Motion), a Cryptocurrency Price Tracker (CoinGecko API), and a CLI Expense Tracker (Bash, SQLite), demonstrating my ability to solve problems across the stack—from interactive UIs to efficient backend systems. Currently deepening my skills in responsive design, accessibility, and scalable architecture, I thrive on turning ideas into clean, performant applications. Let’s connect and create something impactful!
       </p>
     </section>
   );
