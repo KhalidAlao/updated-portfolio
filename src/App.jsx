a9e63570d57846a7d4940600a29eb7ea0530beaa
KhalidAlao <145371547+KhalidAlao@users.noreply.github.com>
2026-02-23 08:02:38 +0000

    Remove NofFound

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from "./Components/About/About";
 import Projects from "./Components/Projects/Projects";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
-import NotFound from "./Components/NotFound/NotFound";
+
 
 
 function App() {
